dc43b69c0e20402051ba6497c5fbfebb55cb7020
Talibiq123 <talibsaeed90@gmail.com>
2024-08-03 21:16:54 +0530

    Routing

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 import http from 'http'
+import dotenv from 'dotenv';
 
-const PORT = 8000
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     // res.write('Hello, World!');
     // res.end();
 
-    // res.setHeader('Content-Type', 'text/html')
+    // res.setHeader(404, {'Content-Type', 'text/html'})
     // res.statusCode = 404;
     // res.end('<h1>Hello, World!</h1>');
 
-    res.writeHead(500, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: 'server error'}));
-    
+    // res.writeHead(500, {'Content-Type': 'application/json'});
+    // res.end(JSON.stringify({message: 'server error'}));
+
     // res.end('Hello, World!');
+
+    try {
+        // Check if request is GET
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.end('<h1>Hello, World!</h1>');
+        } else if (req.url === '/about') {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end('<h1>About websites</h1>');
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'})
+            res.end('<h1>Not Found!</h1>');
+        }
+        } else {
+            throw new Error('Method not found');
+        }
+    } catch (error) {
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.end('<h1>Server Error!</h1>');
+    }
+
+
 
 })
 
@@ -21,3 +47,4 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
     console.log(`server listening on ${PORT}`);
 })
+
